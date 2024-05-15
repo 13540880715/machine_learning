7de8d174e321e54676019f5b4ee34439e8eb33a5
zhaorun <zhaorun@zhaoruns-MacBook-Pro.local>
2024-05-15 18:24:18 +0800

    Initial commit

---
+++ linear_regression/linear_regression.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets,linear_model 
+from sklearn.metrics import mean_squared_error,r2_score
+
+x,y=datasets.load_diabetes(return_X_y=True)
+x=x[:,np.newaxis,2]
+x_train,x_test=x[:-20],x[-20:]
+y_train,y_test=y[:-20],y[-20:]
+
+model=linear_model.LinearRegression()
+model.fit(x_train,y_train)
+
+y_pre=model.predict(x_test)
+
+print(f"""  
+coef_:{model.coef_}
+mean_squared_error:{mean_squared_error(y_test,y_pre)}
+r2_score:{r2_score(y_test,y_pre)}
+""")
+
+
